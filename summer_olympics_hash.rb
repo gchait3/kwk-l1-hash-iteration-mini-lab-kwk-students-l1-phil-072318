c0936bf0027f5bb408f02736fbf16c3a3bdc5af7
Grace T <gtsai64@gmail.com>
2019-03-23 18:10:24 +0000

    Automatically backed up by Learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,18 +1,29 @@
 
+# |Place          | Year      |
+# | ------------- |:---------:|
+# | Sydney        | 2000      |
+# | Athens        | 2004      |
+# | Beijing       | 2008      |
+# | London        | 2012      |
+
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
+olympics_hash = {"Sydney"=>2000, "Athens"=>2004, "Beijing"=>2008, "London"=>2012}
+puts olympics_hash
 end
+create_olympics_hash
+
+# olympics_list.each do |olympics|
+#   olympics_hash [olympics] = olympics_hash[index]
+#   index += 4
+
+#     puts olympics_hash
+#   end
+
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-end
 
-def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-end
-
-def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+new_items["Brazil"] = 2016
+  puts olympics_hash += new_items
 end
